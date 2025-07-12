7897b7b055c16e7de4aa552ba535d3dbf6426043
Adrian Wedd <adrian@adrianwedd.com>
2025-07-12 20:05:52 +1000

    feat(agent): add barge-in support

--- tel3sis/__init__.py
+++ tel3sis/__init__.py
@@ -1,3 +1,3 @@
-from .barge_in import BargeInController
+from .barge_in import BargeInController, enable_barge_in, handle_barge_in
 
-__all__ = ["BargeInController"]
+__all__ = ["BargeInController", "enable_barge_in", "handle_barge_in"]
